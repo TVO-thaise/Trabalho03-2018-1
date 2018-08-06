dbe87759b7e2b6e97c724e31e632be82a307aea3
TVO-thaise <thaisevilaca2013@gmail.com>
2018-08-06 12:46:18 -0300

    oficina adapter

--- app/src/main/java/com/example/samsung/trabalho03_2018_1/ManageOficinaActivity.java
+++ app/src/main/java/com/example/samsung/trabalho03_2018_1/ManageOficinaActivity.java
@@ -1,15 +1,58 @@
 package com.example.samsung.trabalho03_2018_1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-public class ManageOficinaActivity extends AppCompatActivity{
+import com.example.samsung.trabalho03_2018_1.model.Oficina;
+
+import java.util.ArrayList;
+
+public class ManageOficinaActivity extends AppCompatActivity implements ClickRecyclerViewListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage_oficina);
+        setContentView(R.layout.activity_oficina_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarr);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabb);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OficinaListaActivity.this, ManageOficinaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    protected void onResume(){
+        super.onResume();
+        RecyclerView recyclerView = findViewById(R.id.rvOficinas);
+        recyclerView.setAdapter(new OficinaAdapter(this.getOficinas(),this,this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+    public Lista<Oficina> getOficinas(){
+
+        ArrayList<Oficina> list = new ArrayList<>();
+
+        Oficina oficina = new Oficina();
+        oficina.setNome("Nova");
+        oficina.setRua("Marechal Deodoro");
+
+        list.add(oficina);
+
+        return lista;
+    }
+
+    @Override
+    public void onClick(Object object) {
 
     }
 }
