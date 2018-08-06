b1dcc88bbb04076a6ddc89fca0467339a2a703c2
TVO-thaise <thaisevilaca2013@gmail.com>
2018-08-06 12:36:55 -0300

    item_mecanico

---
+++ app/src/main/java/com/example/samsung/trabalho03_2018_1/ClickRecyclerViewListener.java
@@ -0,0 +1,6 @@
+package com.example.samsung.trabalho03_2018_1;
+
+public class ClickRecyclerViewListener {
+
+    void onClick(Object object);
+}
--- app/src/main/java/com/example/samsung/trabalho03_2018_1/MecanicoListaActivity.java
+++ app/src/main/java/com/example/samsung/trabalho03_2018_1/MecanicoListaActivity.java
@@ -3,26 +3,57 @@ package com.example.samsung.trabalho03_2018_1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MecanicoListaActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MecanicoListaActivity extends AppCompatActivity implements ClickRecyclerViewListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mecanico_lista);
-
-        Toolbar toolbar = (toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_mecanico_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener(){
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent intent = new Intent(MecanicoListaActivity.this, ManageMecanicoActivity.class);
                 startActivity(intent);
             }
-        }
+        });
     }
+
+
+    protected void onResume(){
+        super.onResume();
+        RecyclerView recyclerView = findViewById(R.id.rvMecanicos);
+        recyclerView.setAdapter(new MecanicoAdapter(this.getMecanicos(),this,this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+    public List<Mecanico> getMecanicos(){
+
+        ArrayList<Mecanico> list = new ArrayList<>();
+
+        Mecanico mecanico = new Mecanico();
+        mecanico.setNome("João");
+        mecanico.setFuncao("Mecanico");
+
+        list.add(mecanico);
+
+        return list;
+    }
+
+    @Override
+    public void onClick(Object object) {
+
+    }
+
 }
