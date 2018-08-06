6561094617cf2426ebc8ef1a1a622b57ad88975f
TVO-thaise <thaisevilaca2013@gmail.com>
2018-08-06 09:44:30 -0300

    Main Activity”

--- app/src/main/java/com/example/samsung/trabalho03_2018_1/MainActivity.java
+++ app/src/main/java/com/example/samsung/trabalho03_2018_1/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.samsung.trabalho03_2018_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnMecanico, btnOficina;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnMecanico = findViewById(R.id.btnMecanico);
+        this.btnOficina = findViewById(R.id.btnOficina);
+
+        this.btnMecanico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"teste" ,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        this.btnOficina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "teste", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
